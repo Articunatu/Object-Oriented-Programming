65f42fa5ced064f6d09200f9dbbeabae3ea33268
Articunatu <62301779+Articunatu@users.noreply.github.com>
2021-11-10 10:18:32 +0100

    feat: Add child classes

--- 03_-_Polymorphism_Geometry/03_-_Polymorphism_Geometry/Drawing.cs
+++ 03_-_Polymorphism_Geometry/03_-_Polymorphism_Geometry/Drawing.cs
@@ -6,9 +6,57 @@ namespace _03___Polymorphism_Geometry
 {
     class Drawing
     {
-        public double Area()
+        public virtual double Area()
         {
-            return 
+            return 0;
+        }
+    }
+
+    class Circle: Drawing
+    {
+        public double radius { get; set; }
+
+        public Circle()
+        {
+            radius = 4;
+        }
+
+        public override double Area()
+        {
+            return radius * 3.14;
+        }
+    }
+    class Rectangle : Drawing
+    {
+        public double height { get; set; }
+        public double witdth { get; set; }
+
+        public Rectangle()
+        {
+            height = 4;
+            witdth = 5;
+        }
+
+        public override double Area()
+        {
+            return height * witdth;
+        }
+    }
+
+    class Triangle: Drawing
+    {
+        public double triBase { get; set; }
+        public double height { get; set; }
+
+        public Triangle()
+        {
+            height = 7;
+            triBase = 3;
+        }
+
+        public override double Area()
+        {
+            return (height * triBase)/2;
         }
     }
 }
