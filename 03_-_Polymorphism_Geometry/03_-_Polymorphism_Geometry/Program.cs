0884bd90604275c5e78c1f164045e4dd2ede4316
Articunatu <62301779+Articunatu@users.noreply.github.com>
2021-11-10 09:14:01 +0100

    feat: Third assignment startup

---
+++ 03_-_Polymorphism_Geometry/03_-_Polymorphism_Geometry/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03___Polymorphism_Geometry
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
