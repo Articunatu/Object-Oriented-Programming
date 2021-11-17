2a53e4d1c75da8a9b6d1b9ac15d0cac5d26f552d
Christoffer Brandt <christoffer.brant@hotmail.com>
2021-11-17 13:12:00 +0100

    feat: Inheriting class library

--- AssemblerA/FirstAssembly/Class1.cs
+++ AssemblerA/FirstAssembly/Class1.cs
@@ -2,7 +2,17 @@
 
 namespace FirstAssembly
 {
-    public class Class1
+    public class Class1FirstAssmebly
     {
+        protected internal int ID = 120;
+    }
+
+    public class Class2FirstAssmebly
+    {
+        public void Example()
+        {
+            Class1FirstAssmebly A1 = new Class1FirstAssmebly();
+            Console.WriteLine(A1.ID);
+        }         
     }
 }
--- AssemblerA/SecondAssembly/Class1.cs
+++ AssemblerA/SecondAssembly/Class1.cs
@@ -1,8 +1,18 @@
 ﻿using System;
+using FirstAssembly;
 
 namespace SecondAssembly
 {
-    public class Class1
+    public class Class1 : Class1FirstAssmebly
     {
+        public void Exec()
+        {
+            Class1FirstAssmebly C1 = new Class1FirstAssmebly();
+
+            base.ID = 228;
+
+            Class1 class1 = new Class1();
+            class1.ID = 225;
+        }
     }
 }
