f44669ee9b2a8c2d584cccd8dfde54ece7695a5d
Christoffer Brandt <christoffer.brant@hotmail.com>
2021-11-22 18:35:49 +0100

    feat; Generic class

---
+++ 05_-_Generic_Class/05_-_Generic_Class/Check.cs
@@ -0,0 +1,29 @@
+﻿
+namespace _05___Generic_Class
+{
+    public class Check<Unknown>
+    {
+        public bool ToCheck(Unknown firstVar, Unknown secondVar)
+        {
+            
+            if (firstVar.Equals(secondVar))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    public class Film<Unknown> : Check<Unknown>
+    {
+
+    }
+
+    public class TVSeries<Unknown> : Check<Unknown>
+    {
+
+    }
+}
