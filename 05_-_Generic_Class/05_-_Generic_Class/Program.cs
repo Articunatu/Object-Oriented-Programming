67897b1efd5b50dc2913c6c18c68add6500f419b
Christoffer Brandt <christoffer.brant@hotmail.com>
2021-11-25 11:35:02 +0100

    doc: Comment if cases should be TRUE or FALSE

--- 05_-_Generic_Class/05_-_Generic_Class/Program.cs
+++ 05_-_Generic_Class/05_-_Generic_Class/Program.cs
@@ -6,20 +6,20 @@ namespace _05___Generic_Class
     {
         static void Main()
         {
-            TVSeries<string> twoSeasons = new TVSeries<string>();
+            TVSeries<string> twoSeasons = new TVSeries<string>(); //false
             bool isTV = twoSeasons.ToCheck("TV", "Series");
             Console.WriteLine(isTV);
-
-            TVSeries<int> oneSeason = new TVSeries<int>();
+             
+            TVSeries<int> oneSeason = new TVSeries<int>();  //true
             bool isSameNumber = oneSeason.ToCheck(111, 111);
             Console.WriteLine(isSameNumber);
 
             Film<string> overOneHour = new Film<string>();
-            bool isCinema = overOneHour.ToCheck("Cinema", "Cinema");
+            bool isCinema = overOneHour.ToCheck("Cinema", "Cinema"); //true
             Console.WriteLine(isCinema);
 
             Film<int> overTwoHours = new Film<int>();
-            bool isSameRelease = overTwoHours.ToCheck(1975, 1976);
+            bool isSameRelease = overTwoHours.ToCheck(1975, 1976); //false
             Console.WriteLine(isSameRelease);
         }
     }
